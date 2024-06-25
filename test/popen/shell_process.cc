055426b4b7fb86a7a0d0fb5cd276ca4663dc1e3e
mengjie <limengjie@hotmail.com>
2024-06-25 20:00:44 +0800

    finish shell process

--- test/popen/shell_process.cc
+++ test/popen/shell_process.cc
@@ -9,8 +9,10 @@
 #include <chrono>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
 #include <functional>
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <string>
 #include <thread>
@@ -18,31 +20,45 @@
 #define LOG_ERROR std::cerr << std::endl
 #define LOG_INFO std::cout << std::endl
 
-class LogStringStream : public std::ostringstream {
-   public:
-    ~LogStringStream() { LOG_INFO << this->str(); }
-};
+bool check_file_exist(const std::string &file_path) {
+    std::ifstream file(file_path);
+    return file.good();
+}
 
+class LogStringStream : public std::ostringstream {
+public:
+    ~LogStringStream() {
+        auto log_str = this->str();
+        if (log_str.size()) {
+            LOG_INFO << log_str;
+        }
+    }
+
+    void flush_log() {
+        auto log_str = this->str();
+        if (log_str.size()) {
+            LOG_INFO << log_str;
+            this->str("");
+        }
+    }
+};
 class DeferFunction {
-   public:
-    explicit DeferFunction(const std::function<void()> &defer_func)
-        : defer_func_(defer_func) {}
+public:
+    explicit DeferFunction(const std::function<void()> &defer_func) : defer_func_(defer_func) {}
 
     ~DeferFunction() { defer_func_(); }
 
-   private:
+private:
     std::function<void()> defer_func_;
 };
 
 class ShellProcess {
-   public:
+public:
     ShellProcess() = default;
 
     ShellProcess(const std::string &cmd) : pid_(exec(cmd)) {}
 
-    ShellProcess(ShellProcess &&lhs) noexcept : pid_(lhs.pid_) {
-        lhs.pid_ = -1;
-    }
+    ShellProcess(ShellProcess &&lhs) noexcept : pid_(lhs.pid_) { lhs.pid_ = -1; }
 
     ShellProcess &operator=(ShellProcess &&lhs) noexcept {
         if (this != &lhs) {
@@ -57,16 +73,15 @@ class ShellProcess {
     ShellProcess &operator=(const ShellProcess &) = delete;
 
     int terminate() {
-        LogStringStream log_ss;
-        log_ss << __func__ << ", pid: " << pid_;
         if (pid_ > 0) {
+            LogStringStream log_ss;
+            log_ss << __PRETTY_FUNCTION__ << ", pid: " << pid_;
             int killpg_ret = ::killpg(pid_, SIGKILL);
             log_ss << ", killpg_ret: " << killpg_ret;
             std::this_thread::yield();
             if (killpg_ret < 0) {
                 log_ss << ", error: " << strerror(errno);
             }
-
             return waitNoBlock();
         }
         return -1;
@@ -74,16 +89,13 @@ class ShellProcess {
 
     int wait() {
         LogStringStream log_ss;
-        log_ss << __func__ << ", pid: " << pid_;
+        log_ss << __PRETTY_FUNCTION__ << ", pid: " << pid_;
         if (pid_ > 0) {
             int status = -1;
             int waitpid_ret = ::waitpid(pid_, &status, 0);
             pid_ = -1;
-            log_ss << ", status: " << status << ", exited: "
-                   << (WIFEXITED(status) ? WEXITSTATUS(status) : -1)
-                   << ", signal: "
-                   << (WIFSIGNALED(status) ? WTERMSIG(status) : -1)
-                   << ", waitpid_ret: " << waitpid_ret;
+            log_ss << ", status: " << status << ", exited: " << (WIFEXITED(status) ? WEXITSTATUS(status) : -1)
+                   << ", signal: " << (WIFSIGNALED(status) ? WTERMSIG(status) : -1) << ", waitpid_ret: " << waitpid_ret;
             if (waitpid_ret < 0) {
                 log_ss << ", error: " << strerror(errno);
                 return -1;
@@ -97,10 +109,10 @@ class ShellProcess {
 
     pid_t pid() const { return pid_; };
 
-   private:
+private:
     pid_t exec(const std::string &cmd) {
         LogStringStream log_ss;
-        log_ss << __func__ << ", cmd: " << cmd;
+        log_ss << __PRETTY_FUNCTION__ << ", cmd: " << cmd;
         if (cmd.empty()) return -1;
         pid_t pid = fork();
         if (pid == 0) {
@@ -111,8 +123,7 @@ class ShellProcess {
         } else if (pid < 0) {
             LOG_ERROR << "fork failed: " << pid;
         } else {
-            log_ss << ", pid: " << pid
-                   << ", setpgid ret: " << ::setpgid(pid, pid);
+            log_ss << ", pid: " << pid << ", setpgid ret: " << ::setpgid(pid, pid);
         }
         return pid;
     }
@@ -120,36 +131,113 @@ class ShellProcess {
     int waitNoBlock() {
         DeferFunction defer_func([this] { this->pid_ = -1; });
         LogStringStream log_ss;
-        log_ss << __func__ << ", pid: " << pid_;
+        log_ss << __PRETTY_FUNCTION__ << ", pid: " << pid_;
         if (pid_ > 0) {
-            static const int max_retry_times = 5;
+            static const int max_retry_times = 15;
             int status = -1;
             int waitpid_ret = 0;
             int retry_times = max_retry_times;
             do {
                 waitpid_ret = ::waitpid(pid_, &status, WNOHANG);
-                if (retry_times == max_retry_times)
-                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                if (max_retry_times - retry_times < 5)
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
                 else
                     std::this_thread::yield();
 
-            } while (--retry_times > 0 &&
-                     ((waitpid_ret == 0) ||
-                      (waitpid_ret == -1 && errno == EINTR) ||
-                      (waitpid_ret != -1 && !WIFEXITED(status) &&
-                       !WIFSIGNALED(status))));
-            log_ss << ", retry_times: " << retry_times + 1
-                   << ", status: " << status << ", exited: "
-                   << (WIFEXITED(status) ? WEXITSTATUS(status) : -1)
-                   << ", signal: "
-                   << (WIFSIGNALED(status) ? WTERMSIG(status) : -1)
-                   << ", waitpid_ret: " << waitpid_ret;
+            } while (--retry_times > 0 && ((waitpid_ret == 0) || (waitpid_ret == -1 && errno == EINTR) ||
+                                           (waitpid_ret != -1 && !WIFEXITED(status) && !WIFSIGNALED(status))));
+            log_ss << ", retry_times: " << retry_times + 1 << ", status: " << status
+                   << ", exited: " << (WIFEXITED(status) ? WEXITSTATUS(status) : -1)
+                   << ", signal: " << (WIFSIGNALED(status) ? WTERMSIG(status) : -1) << ", waitpid_ret: " << waitpid_ret;
             return status;
         }
         return -1;
     }
 
     pid_t pid_ = -1;
+};
+
+class VoicePlayer {
+public:
+    void playContinuous(const std::string &voice_path) {
+        LogStringStream log_ss;
+        log_ss << __PRETTY_FUNCTION__ << ", path: " << voice_path;
+        if (check_file_exist(voice_path)) {
+            std::lock_guard<std::mutex> lk_shell_process(shell_process_mutex_);
+            shell_process_ = ShellProcess("while [ $PPID -eq $(cut -d ' ' -f 4 /proc/$$/stat) ]; do aplay " + voice_path + "; done");
+            log_ss << ", pid: " << shell_process_.pid();
+            cur_voice_path = voice_path;
+        } else {
+            log_ss << ", not exist";
+        }
+    }
+
+    void playOnce(const std::string &voice_path) {
+        LogStringStream log_ss;
+        log_ss << __PRETTY_FUNCTION__ << ", path: " << voice_path;
+        if (check_file_exist(voice_path)) {
+            std::lock_guard<std::mutex> lk_shell_process(shell_process_mutex_);
+            shell_process_ = ShellProcess("aplay " + voice_path);
+            log_ss << ", pid: " << shell_process_.pid();
+            cur_voice_path = voice_path;
+        } else {
+            log_ss << ", not exist";
+        }
+    }
+
+    void playOnceBlock(const std::string &voice_path) {
+        LogStringStream log_ss;
+        log_ss << __PRETTY_FUNCTION__ << ", path: " << voice_path;
+        if (check_file_exist(voice_path)) {
+            pid_t pid_voice = -1;
+            {
+                std::lock_guard<std::mutex> lk_shell_process(shell_process_mutex_);
+                shell_process_ = ShellProcess("aplay " + voice_path);
+                pid_voice = shell_process_.pid();
+                log_ss << ", pid: " << pid_voice;
+                cur_voice_path = voice_path;
+            }
+            log_ss.flush_log();
+            if (pid_voice > 0) {
+                int status = -1;
+                int waitpid_ret = ::waitpid(pid_voice, &status, 0);  // waitpid阻塞时不持有锁, 其它线程可调用stop提前结束
+                log_ss << "waitpid, status: " << status << ", exited: " << (WIFEXITED(status) ? WEXITSTATUS(status) : -1)
+                       << ", signal: " << (WIFSIGNALED(status) ? WTERMSIG(status) : -1) << ", waitpid_ret: " << waitpid_ret;
+            }
+            {
+                std::lock_guard<std::mutex> lk_shell_process(shell_process_mutex_);
+                log_ss << "cur_pid: " << shell_process_.pid() << ", pid_voice: " << pid_voice;
+                if (shell_process_.pid() == pid_voice) log_ss << ", terminate: " << shell_process_.terminate();
+                cur_voice_path = "";
+            }
+        } else {
+            log_ss << ", not exist";
+        }
+    }
+
+    void stop(const std::string &voice_path) {
+        LogStringStream log_ss;
+        log_ss << __PRETTY_FUNCTION__ << ", voice_path: " << voice_path;
+        std::lock_guard<std::mutex> lk_shell_process(shell_process_mutex_);
+        log_ss << ", cur_voice: " << cur_voice_path;
+        if (voice_path == cur_voice_path) {
+            log_ss << ", terminate: " << shell_process_.terminate();
+            cur_voice_path = "";
+        }
+    }
+
+    void stop() {
+        LogStringStream log_ss;
+        log_ss << __PRETTY_FUNCTION__;
+        std::lock_guard<std::mutex> lk_shell_process(shell_process_mutex_);
+        log_ss << ", terminate: " << shell_process_.terminate();
+        cur_voice_path = "";
+    }
+
+private:
+    std::mutex shell_process_mutex_;
+    ShellProcess shell_process_;
+    std::string cur_voice_path;
 };
 
 bool isProcessRunning(pid_t pid) {
@@ -171,10 +259,34 @@ void func() {
     std::cin >> tmp;
 }
 
-int main() {
-    func();
-    // p1.terminate();
-    int tmp = 0;
+#define WAIT_LINE                                                \
+    std::cout << std::endl << "line: " << __LINE__ << std::endl; \
     std::cin >> tmp;
+
+int main() {
+    int tmp = 0;
+    std::string voice_long{"/home/mengjie/Music/security_alarm.wav"};
+    std::string voice_short{"/home/mengjie/Music/beep_warning.wav"};
+    VoicePlayer p;
+    p.playContinuous(voice_short);
+    WAIT_LINE
+    p.stop();
+    WAIT_LINE
+    p.playOnce(voice_long);
+    WAIT_LINE
+    p.stop(voice_short);
+    WAIT_LINE
+    p.stop(voice_long);
+    WAIT_LINE
+    p.playOnceBlock(voice_short);
+    WAIT_LINE
+    std::thread([&] {
+        sleep(5);
+        p.stop(voice_long);
+    }).detach();
+    p.playOnceBlock(voice_long);
+    WAIT_LINE
+
+    // p1.terminate();
     return 0;
 }
